ab3df9f66f307d5c2accf855be288be872894f2a
Sarah Jundt <sarahjundt@Sarahs-MacBook-Pro-41.local>
2014-12-09 11:18:02 -0800

    Revert "not working: threaded script"
    This reverts commit 6961ff66807288c2f6a77f461ede9c7edb0ad572.

--- YikYak/MultiClassify.java
+++ YikYak/MultiClassify.java
@@ -1,99 +1,44 @@
 import java.io.*;
-import java.util.*;
 
-public class MultiClassify extends Thread{
-	//unigrams, dayOfWeek, header?, length(words), length(chars), uniqueWords, numCapLetters
-	public static final int NUM_FEATURES = 7;
-	private int i;
-	public MultiClassify(int i){
-		this.i = i;
-	}
-	public void run() {
-		System.out.println(""+i);
-		String[] subSet = new String[NUM_FEATURES];
-		for(int b = 0; b < NUM_FEATURES; b++) {
-			subSet[b] = "" + ((i >> b) & 1);
-		}
-		String subsetString = Arrays.toString(subSet);
-		FeatureSelector.main(subSet); //java
-		new ClaremontThread(subsetString).start();
-		new AllThread(subsetString).run();
-	}
+public class MultiClassify {
+	
 	public static void main(String[] args) {
-		//Math.pow(2,numFeatures)
-		for(int i = 0; i < 1; i++) {
-			new MultiClassify(i).start();
-		}
-	}
-
-	private class ClaremontThread extends Thread{
-
-		String subsetString;
-		public ClaremontThread(String subsetString) {
-			this.subsetString = subsetString;
-		}
-		public void run() {
-			PrintWriter cwriter=null;
-			BufferedReader reader;
-			Runtime rt = Runtime.getRuntime();
-			String line;
-			//print features!!
-			try{
-				cwriter = new PrintWriter(new FileWriter("claremontResults.txt"));
-				cwriter.println("features "+subsetString);
-				System.out.println("trying to learn on"+subsetString+"claremont.features_train.txt");
-				Process	pr = rt.exec("svm_learn -z c "+subsetString+"claremont.features_train.txt ../regressionModels/"+subsetString+"claremont.model");
-				pr.waitFor();
-				System.out.println("java done claremont"+subsetString);
-				pr = rt.exec("svm_classify "+subsetString+"claremont.rand.features_test.txt ../regressionModels/"+subsetString+"claremont.model "+subsetString+"claremont.rand.output");
-				pr.waitFor();
-				pr = rt.exec("python ../accuracy.py "+subsetString+"claremont.rand.labels.txt "+subsetString+"claremont.rand.output");
-				pr.waitFor();
-				reader = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-				cwriter.print("rand: ");
-				while((line = reader.readLine()) != null) {
-					cwriter.print(line+"\t");
+		//unigrams, dayOfWeek, postTime, header?, length(words), length(chars), uniqueWords, numCapLetters
+		int numFeatures = 8;
+		PrintWriter writer = null;
+		PrintWriter cwriter= null;
+		try {
+			writer = new PrintWriter(new FileWriter("allSchoolResults.txt"));
+			cwriter = new PrintWriter(new FileWriter("claremontResults.txt"));
+			BufferedReader reader; //Math.pow(2,numFeatures)
+			for(int i = 0; i < Math.pow(2,numFeatures); i++) {
+				System.out.println(""+i);
+				writer.print("features ");
+				cwriter.print("features ");
+				String[] subSet = new String[numFeatures];
+				for(int b = 0; b < numFeatures; b++) {
+					subSet[b] = "" + ((i >> b) & 1);
+					writer.print(subSet[b]+",");
+					cwriter.print(subSet[b]+",");
 				}
+				writer.println();
 				cwriter.println();
-
-				pr = rt.exec("svm_classify "+subsetString+"claremont.time.features_test.txt ../regressionModels/"+subsetString+"claremont.model "+subsetString+"claremont.time.output");
-				pr.waitFor();
-				pr = rt.exec("python ../accuracy.py "+subsetString+"claremont.time.labels.txt "+subsetString+"claremont.time.output");
-				pr.waitFor();
+				FeatureSelector.main(subSet);
+				Runtime rt = Runtime.getRuntime();
+				Process pr = rt.exec("svm_learn -z c all_schools.features_train.txt ../regressionModels/all_schools.model");
 				reader = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-				cwriter.print("time: ");
+				String line;
 				while((line = reader.readLine()) != null) {
-					cwriter.print(line+"\t");
+					System.out.print(line+"\t");
 				}
-				cwriter.println();
-			} catch(Exception e){
-				e.printStackTrace();
-			} finally {
-				cwriter.close();
-				System.out.println(subsetString+"done");
-			}
-		}
-	}
-	private class AllThread extends Thread{
-		String subsetString, line;
-		public AllThread(String subsetString) {
-			this.subsetString = subsetString;
-		}
-		public void run() {
-			BufferedReader reader;
-			PrintWriter writer=null;
-			Runtime rt = Runtime.getRuntime();
-			try{
-				//print features!!
-				writer = new PrintWriter(new FileWriter("allSchoolResults.txt"));
-				writer.println("features "+subsetString);
-				System.out.println("trying to learn on"+subsetString+"all_schools.features_train.txt");
-				Process pr = rt.exec("svm_learn -z c "+subsetString+"all_schools.features_train.txt ../regressionModels/"+subsetString+"all_schools.model");
 				pr.waitFor();
-
-				pr = rt.exec("svm_classify "+subsetString+"all_schools.rand.features_test.txt ../regressionModels/"+subsetString+"all_schools.model "+subsetString+"all_schools.rand.output");
+				pr = rt.exec("svm_classify all_schools.rand.features_test.txt ../regressionModels/all_schools.model all_schools.rand.output");
+				reader = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+				while((line = reader.readLine()) != null) {
+					System.out.print(line+"\t");
+				}
 				pr.waitFor();
-				pr = rt.exec("python ../accuracy.py "+subsetString+"all_schools.rand.labels.txt "+subsetString+"all_schools.rand.output");
+				pr = rt.exec("python ../accuracy.py all_schools.rand.labels.txt all_schools.rand.output");
 				pr.waitFor();
 				reader = new BufferedReader(new InputStreamReader(pr.getInputStream()));
 				writer.print("rand: ");
@@ -102,22 +47,56 @@ public class MultiClassify extends Thread{
 				}
 				writer.println();
 
-				pr = rt.exec("svm_classify "+subsetString+"all_schools.time.features_test.txt ../regressionModels/"+subsetString+"all_schools.model "+subsetString+"all_schools.time.output");
+				pr = rt.exec("svm_classify all_schools.time.features_test.txt ../regressionModels/all_schools.model all_schools.time.output");
 				pr.waitFor();
-				pr = rt.exec("python ../accuracy.py "+subsetString+"all_schools.time.labels.txt "+subsetString+"all_schools.time.output");
+				pr = rt.exec("python ../accuracy.py all_schools.time.labels.txt all_schools.time.output");
 				pr.waitFor();
 				reader = new BufferedReader(new InputStreamReader(pr.getInputStream()));
 				writer.print("time: ");
 				while((line = reader.readLine()) != null) {
 					writer.print(line+"\t");
 				}
+				pr = rt.exec("svm_learn -z c claremont.features_train.txt ../regressionModels/claremont.model");
+				reader = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+				while((line = reader.readLine()) != null) {
+					System.out.print(line+"\t");
+				}
+				pr.waitFor();
+				pr = rt.exec("svm_classify claremont.rand.features_test.txt ../regressionModels/claremont.model claremont.rand.output");
+				reader = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+				while((line = reader.readLine()) != null) {
+					System.out.print(line+"\t");
+				}
+				pr.waitFor();
+				pr = rt.exec("python ../accuracy.py claremont.rand.labels.txt claremont.rand.output");
+				pr.waitFor();
+				reader = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+				cwriter.print("rand: ");
+				while((line = reader.readLine()) != null) {
+					cwriter.print(line+"\t");
+				}
+				cwriter.println();
+
+				pr = rt.exec("svm_classify claremont.time.features_test.txt ../regressionModels/claremont.model claremont.time.output");
+				pr.waitFor();
+				pr = rt.exec("python ../accuracy.py claremont.time.labels.txt claremont.time.output");
+				pr.waitFor();
+				reader = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+				cwriter.print("time: ");
+				while((line = reader.readLine()) != null) {
+					cwriter.print(line+"\t");
+				}
+				cwriter.println();
 				writer.println();
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				writer.close();
-				System.out.println(subsetString+"done");
 			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			System.out.println("hi");
+			writer.close();
+			cwriter.close();
 		}
 	}
 }
